70a842e7e95e32a1b7f4ea81f0dce00d03f3eb54
Aileen Xin <snowjackband@gmail.com>
2021-04-08 11:25:15 +1000

    summary

---
+++ contact-handler.php
@@ -0,0 +1,14 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $firstName = $_POST['first name'];
+    $lastName = $_POST['last name'];
+    $mailFrom = $_POST['email'];
+    $message = $_POST['message'];
+
+    $mailTo ="contact@aileenxin.com";
+    $headers ="From: ".$mailFrom;
+    $txt ="You have received an email from ".$firstName + $lastName.".\n\n".$message;
+
+    mail($mailTo, $txt, $headers);
+}
